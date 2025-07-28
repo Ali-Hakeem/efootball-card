656c54e1d339599ed0ab75ec8305b483a0293cfd
unknown <Ali-Hakeem@github.com>
2025-07-28 18:42:37 +0700

    first v

--- pages/_document.js
+++ pages/_document.js
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <body>
+      <body style={{ backgroundColor: "#121212", color: "#ffffff" }}>
         <Main />
         <NextScript />
       </body>
